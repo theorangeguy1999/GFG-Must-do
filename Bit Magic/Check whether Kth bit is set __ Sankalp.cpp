e1d35d83041ad8b43c0ebddeb3d37cd493b80033
theorangeguy1999 <31373874+theorangeguy1999@users.noreply.github.com>
2020-08-22 19:14:29 +0530

    Create Check whether Kth bit is set __ Sankalp.cpp

---
+++ Bit Magic/Check whether Kth bit is set __ Sankalp.cpp
@@ -0,0 +1,44 @@
+  
+#include<bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+#define ff first
+#define ss second
+#define pb push_back
+#define INF 1000000000
+#define MOD 1000000007
+#define int long long
+#define un_map unordered_map
+#define un_set unordered_set
+
+using namespace std;
+
+int X[] = {-1,-1,0,1,1,1,0,-1};
+int Y[] = {0,1,1,1,0,-1,-1,-1};
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin>>t;
+
+  while(t--)
+  {
+    int n,k;
+    cin>>n>>k;
+
+    int bit_to_check = 1<<k;
+
+    if(n&bit_to_check)
+      cout<<"Yes"<<endl;
+    else
+      cout<<"No"<<endl;
+
+  }
+
+
+  return 0;
+
+}
