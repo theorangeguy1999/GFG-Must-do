f06a41295b2bc65d1105c87246a439c217e60495
theorangeguy1999 <31373874+theorangeguy1999@users.noreply.github.com>
2020-08-22 18:50:58 +0530

    Create Find first set bit __ Sankalp.cpp

---
+++ Bit Magic/Find first set bit __ Sankalp.cpp
@@ -0,0 +1,55 @@
+  
+#include<bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+#define ff first
+#define ss second
+#define pb push_back
+#define INF 1000000000
+#define MOD 1000000007
+#define int long long
+#define un_map unordered_map
+#define un_set unordered_set
+
+using namespace std;
+
+int X[] = {-1,-1,0,1,1,1,0,-1};
+int Y[] = {0,1,1,1,0,-1,-1,-1};
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin>>t;
+
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int cnt = 1;
+    bool cond = false;
+
+    while(n!=0)
+    {
+      if(n&1 == 1)
+      {
+        cond = true;
+        break;
+      }
+      n = n>>1;
+      cnt++;
+    }
+
+    if(cond)
+      cout<<cnt<<endl;
+    else
+      cout<<0<<endl;
+
+  }
+
+
+  return 0;
+
+}
