67dfbc1766a395ce573964be592ab5705079063b
theorangeguy1999 <31373874+theorangeguy1999@users.noreply.github.com>
2020-08-24 20:11:29 +0530

    Create Swap all odd and even bits __ Sankalp.cpp

---
+++ Bit Magic/Swap all odd and even bits __ Sankalp.cpp
@@ -0,0 +1,44 @@
+  
+#include<bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+#define ff first
+#define ss second
+#define pb push_back
+#define INF 1000000000
+#define MOD 1000000007
+#define int long long
+#define un_map unordered_map
+#define un_set unordered_set
+
+using namespace std;
+
+int X[] = {-1,-1,0,1,1,1,0,-1};
+int Y[] = {0,1,1,1,0,-1,-1,-1};
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin>>t;
+
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int even = n & 0xAAAAAAAA;
+    int odd = n & 0x55555555;
+    even = even>>1;
+    odd = odd<<1;
+
+    int ans = even | odd;
+
+    cout<<ans<<endl;
+  }
+
+
+  return 0;
+
+}
